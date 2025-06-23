0206d7cd60256c9d4ebd17696889277c18276970
Hemanth Sai Maheswarla <hemanthsaimaheswarla@Hemanths-MacBook-Pro.local>
2025-06-24 01:48:33 +0530

    Revert "adding support for automation testing of downloading old objects after upgrade"
    This reverts commit 3827cc0ea881d0c0f9aaf4a933cb5b4dc888614d.

--- tests/rgw/sanity_rgw.py
+++ tests/rgw/sanity_rgw.py
@@ -204,19 +204,16 @@ def run(ceph_cluster, **kw):
 
     if run_io_verify:
         log.info("running io verify script")
-        verify_io_info_configs = config.get(
-            "verify-io-info-configs", [config_file_name]
+        verify_status = exec_from.exec_command(
+            cmd=f"sudo {python_cmd} "
+            + test_folder_path
+            + lib_dir
+            + f"read_io_info.py -c {config_file_name}",
+            long_running=True,
         )
-        for io_config in verify_io_info_configs:
-            verify_status = exec_from.exec_command(
-                cmd=f"sudo {python_cmd} "
-                + test_folder_path
-                + lib_dir
-                + f"read_io_info.py -c {io_config}",
-                long_running=True,
-            )
-            log.info(f"verify io status code is : {verify_status}")
-            if verify_status != 0:
-                raise Exception(f"verify io failed")
+        log.info(f"verify io status code is : {verify_status}")
+        if verify_status != 0:
+            log.error(verify_status)
+            return verify_status
 
     return test_status
